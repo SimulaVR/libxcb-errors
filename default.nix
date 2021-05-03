e821ff72ad407b445b6cdf33fb48b0d210c3fddc
George Singer <george.w.singer@gmail.com>
2021-05-03 18:29:57 -0500

    Fork & nixify.

---
+++ default.nix
@@ -0,0 +1,1 @@
+(import <nixpkgs> {}).callPackage ./libxcb-errors.nix { }
---
+++ libxcb-errors.nix
@@ -0,0 +1,17 @@
+{ stdenv, autoreconfHook, xorg, libbsd, pkg-config, python310}:
+
+stdenv.mkDerivation rec {
+  name = "libxcb-errors-${version}";
+  version = "0.0.0";
+
+  buildInputs = [ autoreconfHook xorg.libxcb xorg.libXau xorg.libXdmcp libbsd xorg.utilmacros pkg-config python310 xorg.xcbproto ];
+
+  src = ./.;
+
+  meta = {
+    description = "Allow XCB errors to print less opaquely";
+    homepage = https://github.com/SimulaVR/libxcb-errors;
+    license = stdenv.lib.licenses.mit;
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
